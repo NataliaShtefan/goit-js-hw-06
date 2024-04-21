c7cb9b9a6dacbaf2de114c78cc52c83b1a7d2ffc
NataliaShtefan <153209330+NataliaShtefan@users.noreply.github.com>
2024-04-21 18:53:31 +0200

    change 1

---
+++ js/task-2.js
@@ -0,0 +1,42 @@
+class Storage {
+    #item
+    constructor (item) {
+this.#item = item
+    }
+
+    getItems() {
+return this.#item
+    }
+
+ addItem(newItem) {
+   this.#item.push(newItem)
+}
+
+removeItem(itemToRemove) {
+    if (this.#item.includes(itemToRemove)) {
+        this.#item.splice(itemToRemove, 1)
+    }
+}
+
+}
+
+
+
+
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+
+console.log(storage)
+
+
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
